fefa6f3415a2d6abf7a477832a77249f9939392d
Abdullah Hassan <65224472+abdullah12-alt@users.noreply.github.com>
2025-10-03 16:04:52 +0500

    cors error

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ const app = express();
 
 //middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://ec2-16-171-132-75.eu-north-1.compute.amazonaws.com", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 
 // Routes
